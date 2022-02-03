46f9a74ee8abbe423ba94d35557452179457ae84
Alexis cabral <cabralalexis1@gmail.com>
2022-02-03 18:10:21 -0300

    ejercicio 3

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -1,12 +1,19 @@
+/*ALEXIS RAMON CABRAL BARRIOS */
+/*EJERCICIO N3 ENTRADA Y SALIDA.JS*/
+
 /*
 Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	var nombreIngresado;
-	nombreIngresado=txtIdNombre.value;
-	alert("ok");
+	/*txtIdNombre */
+	/*NO UTILIZAR*/
 
+	let nombreIngresado;
+
+	nombreIngresado=document.getElementById("txtIdNombre").value;
+
+	alert(nombreIngresado)
 }
 
 
